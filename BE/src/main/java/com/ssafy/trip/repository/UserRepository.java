28b20f940209c9fde18f2d66395a15de7983859d
chanki00 <rlacksrl00@gmail.com>
2025-05-20 15:32:43 +0900

    refactor: mybatis user 수정

--- BE/src/main/java/com/ssafy/trip/repository/UserRepository.java
+++ BE/src/main/java/com/ssafy/trip/repository/UserRepository.java
@@ -2,10 +2,13 @@ package com.DB_PASSWORD_REDACTED.trip.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.DB_PASSWORD_REDACTED.trip.dto.user.UserDto;
 import com.DB_PASSWORD_REDACTED.trip.dto.user.UserInfoDto;
 import com.DB_PASSWORD_REDACTED.trip.dto.user.UserPwDto;
 
+@Mapper
 public interface UserRepository {
 	public int insertUser(UserDto user);
 	public UserDto selectUser(Long id);
