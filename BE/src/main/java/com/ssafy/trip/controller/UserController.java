0134037fe3191629ae36fb30f4145c5f3b00f1d3
chanki00 <rlacksrl00@naver.com>
2025-05-21 01:34:40 +0900

    feat: UserContoller - update, delete 추가

--- BE/src/main/java/com/ssafy/trip/controller/UserController.java
+++ BE/src/main/java/com/ssafy/trip/controller/UserController.java
@@ -7,12 +7,18 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.DB_PASSWORD_REDACTED.trip.dto.user.UserDto;
+import com.DB_PASSWORD_REDACTED.trip.dto.user.UserInfoDto;
+import com.DB_PASSWORD_REDACTED.trip.dto.user.UserPwDto;
 import com.DB_PASSWORD_REDACTED.trip.service.UserService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -41,8 +47,85 @@ public class UserController {
 	@Operation(summary = "모든 사용자 정보 조회")
 	public ResponseEntity<List<UserDto>> selectAllUser() {
 		
-		
-		
-		return null;
+		List<UserDto> userList = uService.selectAllUser();
+				
+		return ResponseEntity.ok(userList);
 	}
+	
+	@PostMapping("")
+	@Operation(summary = "멤버 삽입")
+	public ResponseEntity<Map<String, Object>> insertUser(@RequestBody UserDto user) {
+		
+	    int cnt = uService.insertUser(user);
+
+	    Map<String, Object> map = new HashMap<>();
+	    
+	    if (cnt == 0) {
+	    	map.put("message", "등록 실패");
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(map);
+	    }
+	    
+	    map.put("message", "등록 성공");
+	    map.put("userId", user.getId());
+	    
+	    return ResponseEntity.status(HttpStatus.CREATED).body(map);
+	}
+	
+	@PatchMapping("/info/{id}")
+	@Operation(summary = "멤버 수정")
+	public ResponseEntity<Map<String, Object>> updateInfo(@PathVariable Long id, @RequestBody UserInfoDto user) {
+		user.setId(id);
+		
+		int cnt = uService.updateInfo(user);
+		
+		Map<String, Object> map = new HashMap<>();
+		
+		if (cnt == 0) {
+			map.put("message", "멤버 정보 수정 실패");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(map);
+		}
+		
+	    map.put("message", "멤버 정보 수정 성공");
+	    map.put("userId", user.getId());
+		
+	    return ResponseEntity.ok(map);
+	}
+	
+	@PatchMapping("/pw/{id}")
+	@Operation(summary = "비밀번호 수정")
+	public ResponseEntity<Map<String, Object>> updatePw(@PathVariable Long id, @RequestBody UserPwDto user) {
+		user.setId(id);
+		
+		int cnt = uService.updatePw(user);
+		
+		Map<String, Object> map = new HashMap<>();
+		
+		if (cnt == 0) {
+			map.put("message", "수정 실패");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(map);
+		}
+		
+	    map.put("message", "비밀번호 수정 성공");
+	    map.put("userId", user.getId());
+		
+	    return ResponseEntity.ok(map);
+	}
+	
+	@DeleteMapping("/{id}")
+	@Operation(summary = "멤버 삭제")
+	public ResponseEntity<Map<String, Object>> deleteUser(@PathVariable Long id) {
+		
+		int cnt = uService.deleteUser(id);
+		
+		Map<String, Object> map = new HashMap<>();
+		
+		if (cnt == 0) {
+			map.put("message", "삭제 실패");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(map);
+		}
+		
+		map.put("message", "삭제 성공");
+		return ResponseEntity.ok(map);
+	}
+	
 }
